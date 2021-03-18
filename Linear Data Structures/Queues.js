90d8801ae8bd142bf1325dc562af0cdc5aad1e9b
JM <jnmrles@gmail.com>
2021-03-17 22:33:08 -0500

    updated read.me

---
+++ Linear Data Structures/Queues.js
@@ -0,0 +1,8 @@
+// A Queue is a linear collection of nodes that exclusively adds nodes to the tail (enqueues), and removes (dequeues) nodes from the head of the queue
+// EX. a grocery store line, people leave at the front of the line and people join the line at the back of the line
+
+// Queues provide three methods for interaction:
+
+// Enqueue - adds data to the “back” or end of the queue
+// Dequeue - provides and removes data from the “front” or beginning of the queue
+// Peek - reveals data from the “front” of the queue without removing it
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Data-Structures-and-Algorithms
+## Data Structures and Algorithms
+
+A Directory for all my Big-O and Datastructure problems!
+
+
+## Project Status
+
+Data Structures and Algorithms are seperated by types found in their perspective folders
+
+Here you you can find quick notes on what that specific data structure is, how to implement it with javascript, and includes practice problems that inludes time and space complexity analysis for their solutions
+
