bae4637a3c5b88fedc9cb712bea8f3ceea640077
JM <jnmrles@gmail.com>
2021-03-27 05:46:40 -0500

    webNavgation helper functions and intialized stacks for back and next pages

--- Linear Data Structures/webNavigator/script.js
+++ Linear Data Structures/webNavigator/script.js
@@ -1,13 +1,37 @@
+// This project, Web Navigator, simulates the navigational operations of a web browser such as :
+// opening a new web page,
+// navigating back a page and
+// going forward a page. This uses the Stack class to maintain the history of visited pages with a backPages stack and a nextPages stack.
+
 const Stack = require("./Stack.js");
 const prompt = require("prompt-sync")();
 // ------------------------------
 // Initialization
 // ------------------------------
-
+const backPages = new Stack();
+const nextPages = new Stack();
+let currentPage = "Start Page";
 // ------------------------------
 // Helper Functions
 // ------------------------------
+const showCurrentPage = (action) => {
+  console.log(`\n${action}`);
+  console.log(`Current page = ${currentPage}`);
+  console.log("Back page = ", backPages.peek());
+  console.log("Next page = ", nextPages.peek());
+};
 
+const newPage = (page) => {
+  backPages.push(currentPage);
+  currentPage = page;
+
+  // clear the nextPages stack
+  while (!nextPages.isEmpty()) {
+    nextPages.pop();
+  }
+
+  showCurrentPage("NEW: ");
+};
 /*
  * The following strings are used to prompt the user
  */
