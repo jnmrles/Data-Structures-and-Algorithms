9d074cb0af1e61122a37958365859738798e4e49
JM <jnmrles@gmail.com>
2021-04-03 23:58:58 -0500

    added HashMap class

---
+++ HashMaps/HashMap.js
@@ -0,0 +1,17 @@
+// ! Hash maps map keys to their related values, and are one of the most efficient data structures when it comes to retrieving stored data. This is because the key associated with every value added allows for faster retrieval later on.t hash maps will often be the most efficient data structure for that scenario.
+
+class HashMap {
+  constructor(size = 0) {
+    this.hashmap = new Array(size).fill(null);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
+    }
+    return hashCode;
+  }
+}
+
+module.exports = HashMap;
