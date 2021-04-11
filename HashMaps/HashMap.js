aa833ec42a0f4f325afd5fd58a9a609bfac695af
JM <jnmrles@gmail.com>
2021-04-11 00:19:01 -0500

    added HashMap class with complete methods for retrieval,hashing, and assignment

--- HashMaps/HashMap.js
+++ HashMaps/HashMap.js
@@ -1,8 +1,10 @@
 // ! Hash maps map keys to their related values, and are one of the most efficient data structures when it comes to retrieving stored data. This is because the key associated with every value added allows for faster retrieval later on.t hash maps will often be the most efficient data structure for that scenario.
 
+const LinkedList = require("./LinkedList");
+const Node = require("./Node");
 class HashMap {
   constructor(size = 0) {
-    this.hashmap = new Array(size).fill(null);
+    this.hashmap = new Array(size).fill(null).map(() => new LinkedList());
   }
 
   hash(key) {
@@ -11,6 +13,42 @@ class HashMap {
       hashCode += hashCode + key.charCodeAt(i);
     }
     return hashCode % this.hashmap.length;
+  }
+
+  assign(key, value) {
+    const arrayIndex = this.hash(key);
+    const linkedList = this.hashmap[arrayIndex];
+    console.log(`Storing ${value} at index ${arrayIndex}`);
+    if (linkedList.head === null) {
+      linkedList.addToHead({ key, value });
+      return;
+    }
+    let current = linkedList.head;
+    while (current) {
+      if (current.data.key === key) {
+        current.data = { key, value };
+      }
+      if (!current.next) {
+        current.next = new Node({ key, value });
+        break;
+      }
+      current = current.next;
+    }
+  }
+
+  retrieve(key) {
+    const arrayIndex = this.hash(key);
+    let current = this.hashmap[arrayIndex].head;
+    while (current) {
+      if (current.data.key === key) {
+        console.log(
+          `\nRetrieving ${current.data.value} from index ${arrayIndex}`
+        );
+        return current.data.value;
+      }
+      current = current.next;
+    }
+    return null;
   }
 }
 
